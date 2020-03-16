306607274a3f488c8960fca8dd0e0076d9928e8a
Glitch (plum-ivory-scorpio) <none>
2020-03-16 20:37:18 +0000

    ☄️🍞 Checkpoint ./server.js:12004567/116

--- server.js
+++ server.js
@@ -18,6 +18,11 @@ app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
 });
 
+app.post('/api/fileanalyse', (req, res) => {
+  console.log(req, req.query.inputfield);
+  res.send('???');
+})
+
 app.listen(process.env.PORT || 3000, function () {
   console.log('Node.js listening ...');
 });
