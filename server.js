621bff7cf1c59fdcded6c74cb5901f6e7a1cbf2a
Glitch (plum-ivory-scorpio) <none>
2020-07-24 22:34:11 +0000

    🌘🌇 Checkpoint ./server.js:19752745/1017

--- server.js
+++ server.js
@@ -5,8 +5,18 @@ var cors = require('cors');
 
 // require and use "multer"...
 var multer = require("multer");
-var storage = multer.memoryStorage();
+// var storage = multer.memoryStorage();
+// var storage = multer.diskStorage({});
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, '/public')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now() + ".pdf")
+  }
+})
 var upload = multer({storage: storage, limits: {fileSize: 30000000}});
+// var upload = multer({ dest: 'public/' })
 var PDFImage = require("pdf-image").PDFImage;
 var fs = require('fs');
 
@@ -26,16 +36,14 @@ app.get('/hello', function(req, res){
 
 app.post('/api/fileanalyse', upload.single('upfile'), (req, res, body) => {
   // console.log(req.file.fieldname, req.file.mimetype, req.file.size);
-  fs.writeFile("tmp/thingy.pdf", body, 'binary', ()=>{
-      var pdfImage = new PDFImage("tmp/thingy.pdf");
-      pdfImage.convertPage(0).then(function (imagePath) {
-        // 0-th page (first page) of the slide.pdf is available as slide-0.png
-        fs.existsSync("/tmp/slide-0.png") // => true
-        // res.send({name: req.file.originalname, type: req.file.mimetype, size: req.file.size});
-        res.send("Ok")
-      });
-    res.send("Ok")
-  })
+  // res.send(req.file.path);
+    var pdfImage = new PDFImage(req.file.path);
+    pdfImage.convertPage(0).then(function (imagePath) {
+      // 0-th page (first page) of the slide.pdf is available as slide-0.png
+      fs.existsSync("/tmp/slide-0.png") // => true
+      // res.send({name: req.file.originalname, type: req.file.mimetype, size: req.file.size});
+      res.send("Ok")
+    });
 })
 
 app.listen(process.env.PORT || 3000, function () {
