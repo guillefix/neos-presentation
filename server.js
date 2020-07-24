459033745637db644265425eab3b1a5a60843e6e
Glitch (plum-ivory-scorpio) <none>
2020-07-24 22:23:09 +0000

    👱🌚 Checkpoint ./server.js:19752745/423

--- server.js
+++ server.js
@@ -24,16 +24,17 @@ app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
 });
 
-app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res, body) => {
   // console.log(req.file.fieldname, req.file.mimetype, req.file.size);
-  fs.writeFile("thingy.pdf", req.file, ()=>{
-      var pdfImage = new PDFImage("thingy.pdf");
+  fs.writeFile("tmp/thingy.pdf", body, 'binary', ()=>{
+      var pdfImage = new PDFImage("tmp/thingy.pdf");
       pdfImage.convertPage(0).then(function (imagePath) {
         // 0-th page (first page) of the slide.pdf is available as slide-0.png
         fs.existsSync("/tmp/slide-0.png") // => true
         // res.send({name: req.file.originalname, type: req.file.mimetype, size: req.file.size});
         res.send("Ok")
-});
+      });
+    res.send("Ok")
   })
 })
 
