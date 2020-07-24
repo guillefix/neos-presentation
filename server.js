2bf721a34ddcbaff262a45d4edab13d3a0be1741
Glitch (plum-ivory-scorpio) <none>
2020-07-24 22:50:20 +0000

    🕋🦀 Checkpoint ./server.js:19752745/1171

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var multer = require("multer");
 // var storage = multer.diskStorage({});
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, '/public')
+    cb(null, 'public')
   },
   filename: function (req, file, cb) {
     cb(null, file.fieldname + '-' + Date.now() + ".pdf")
@@ -17,9 +17,17 @@ var storage = multer.diskStorage({
 })
 var upload = multer({storage: storage, limits: {fileSize: 30000000}});
 // var upload = multer({ dest: 'public/' })
-var PDFImage = require("pdf-image").PDFImage;
 var fs = require('fs');
 
+const PDF2Pic = require("pdf2pic");
+ 
+const pdf2pic = new PDF2Pic({
+  density: 100,           // output pixels per inch
+  savename: "untitled",   // output file name
+  savedir: "./images",    // output file location
+  format: "png",          // output file format
+  size: "600x600"         // output size in pixels
+});
 
 var app = express();
 
@@ -37,13 +45,22 @@ app.get('/hello', function(req, res){
 app.post('/api/fileanalyse', upload.single('upfile'), (req, res, body) => {
   // console.log(req.file.fieldname, req.file.mimetype, req.file.size);
   // res.send(req.file.path);
-    var pdfImage = new PDFImage(req.file.path);
-    pdfImage.convertPage(0).then(function (imagePath) {
-      // 0-th page (first page) of the slide.pdf is available as slide-0.png
-      fs.existsSync("/tmp/slide-0.png") // => true
-      // res.send({name: req.file.originalname, type: req.file.mimetype, size: req.file.size});
-      res.send("Ok")
-    });
+    // var PDFImage = require("pdf-image").PDFImage;
+    // var pdfImage = new PDFImage(req.file.path);
+    // pdfImage.convertPage(0).then(function (imagePath) {
+    //   // 0-th page (first page) of the slide.pdf is available as slide-0.png
+    //   // fs.existsSync("/tmp/slide-0.png") // => true
+    //   // res.send({name: req.file.originalname, type: req.file.mimetype, size: req.file.size});
+    //   // res.send("Ok")
+    //   res.sendFile(imagePath);
+    // });
+  pdf2pic.convert(req.file.path).then((resolve) => {
+  console.log("image converter successfully!");
+ 
+  return resolve;
+  res.send("ok")
+  });
+
 })
 
 app.listen(process.env.PORT || 3000, function () {
